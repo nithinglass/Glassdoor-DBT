8e661c22c471065632cc966bb9f4b21ccf34648c
Nithin Thakur <nithin.thakur@glassdoor.com>
2022-06-10 08:01:25 +0000

    Before hack commit

--- models/marts/drop_tables.sql
+++ models/marts/drop_tables.sql
@@ -9,7 +9,6 @@
 
 {% for relation in relations_to_drop %}
     {% set drop_command -%}
-    -- drop {{ relation.type }} {{ relation }} cascade;
     {%- endset %}
     {% do log(drop_command, info=True) %}
     {% do sql_to_execute.append(drop_command) %}
